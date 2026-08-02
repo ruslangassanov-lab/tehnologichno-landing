8e6fd6861f95da0563f71b3ad3c371b9195bf0e1
Neowave Claude Worker <claude-worker@neowave.local>
2026-08-02 19:07:25 +0000

    pages: версия Антона + адаптив Руслан ВАУ

--- docs/switcher.js
+++ docs/switcher.js
@@ -5,6 +5,6 @@
   bar.id = "vswitch"; bar.setAttribute("aria-label","Переключатель версий");
   bar.innerHTML = \x27<span>Версия:</span>\x27 +
     \x27<a href="../anton/" \x27+(here==="anton"?\x27aria-current="page"\x27:"")+\x27>Макет Антона</a>\x27 +
-    \x27<a href="../ruslan/" \x27+(here==="ruslan"?\x27aria-current="page"\x27:"")+\x27>Редизайн Руслана</a>\x27;
+    \x27<a href="../ruslan/" \x27+(here==="ruslan"?\x27aria-current="page"\x27:"")+\x27>Руслан ВАУ</a>\x27;
   document.body.insertBefore(bar, document.body.firstChild);
 })();
