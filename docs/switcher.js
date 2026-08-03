d8b1a98e1cabb9469d8c0159121f5683f940f9fb
RGik <ruslan.gassanov@gmail.com>
2026-08-03 16:31:13 +0300

    fix: switcher.js syntax bug (\x27 literals) and point to /hero/ instead of stale /ruslan/

--- docs/switcher.js
+++ docs/switcher.js
@@ -1,10 +1,10 @@
 (function(){
-  var here = location.pathname.indexOf("/ruslan") > -1 ? "ruslan" : "anton";
+  var here = location.pathname.indexOf("/hero") > -1 ? "hero" : "anton";
   try{ localStorage.setItem("tehno_version", here); }catch(e){}
   var bar = document.createElement("nav");
   bar.id = "vswitch"; bar.setAttribute("aria-label","Переключатель версий");
-  bar.innerHTML = \x27<span>Версия:</span>\x27 +
-    \x27<a href="../anton/" \x27+(here==="anton"?\x27aria-current="page"\x27:"")+\x27>Макет Антона</a>\x27 +
-    \x27<a href="../ruslan/" \x27+(here==="ruslan"?\x27aria-current="page"\x27:"")+\x27>Руслан ВАУ</a>\x27;
+  bar.innerHTML = '<span>Версия:</span>' +
+    '<a href="../anton/" '+(here==="anton"?'aria-current="page"':"")+'>Макет Антона</a>' +
+    '<a href="../hero/" '+(here==="hero"?'aria-current="page"':"")+'>Руслан ВАУ</a>';
   document.body.insertBefore(bar, document.body.firstChild);
 })();
